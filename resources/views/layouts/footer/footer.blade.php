23e59e24b25d6033c953cc52759752216b21b814
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-09-07 11:46:03 -0300

    footer

--- resources/views/layouts/footer/footer.blade.php
+++ resources/views/layouts/footer/footer.blade.php
@@ -14,7 +14,7 @@
   
     <!-- Copyright -->
     <div class="text-center p-3" >
-     <span class="text1Style"> © 2020 Copyright:</span>
+     <span class="text1Style"> © 2023 Copyright:</span>
       <a class=" linkStyle" href="https://www.linkedin.com/in/frankie-silva-santos-5968451ab/"><span class="spanLink">Frankie Yoogan</span></a>  <a class="btn btn-outline btn-floating m-1 " href="https://github.com/FranzieYoogan" role="button"
       ><span class="spanIcon"><i class="fab fa-github linkIconStyle"></span></i
     ></a>
